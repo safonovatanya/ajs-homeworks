4f33ff8da5ee4990fc9008a8687851288cd2c1c4
a11 <a11@Tanya-3.local>
2019-12-08 18:45:59 +0300

    one

---
+++ modules/src/js/game.js
@@ -0,0 +1,18 @@
+import Character from './domain';
+
+export default class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+export { GameSavingData, readGameSaving, writeGameSaving };
